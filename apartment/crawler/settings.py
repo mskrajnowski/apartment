7dd3d69ccaedace54731719ab5aca61fcf3820a6
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2018-03-08 23:17:44 +0100

    Cleaned up settings

--- apartment/crawler/settings.py
+++ apartment/crawler/settings.py
@@ -10,89 +10,50 @@ from apartment.crawler.shub import environment
 environment.load_env()
 
 # Scrapy settings for apartment project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://doc.scrapy.org/en/latest/topics/settings.html
-#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+# https://doc.scrapy.org/en/latest/topics/settings.html
 
 BOT_NAME = 'apartment'
 
 SPIDER_MODULES = ['apartment.crawler.spiders']
 NEWSPIDER_MODULE = 'apartment.crawler.spiders'
 
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
+# TODO: name the bot :)
 #USER_AGENT = 'apartment (+http://www.yourdomain.com)'
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+COOKIES_ENABLED = False
+REDIRECT_ENABLED = True
+RETRY_ENABLED = False   # Will be handled by Frontera
+TELNETCONSOLE_ENABLED = False
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
-
-# Enable or disable spider middlewares
-# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 SPIDER_MIDDLEWARES = {
-#    'apartment.crawler.middlewares.ApartmentSpiderMiddleware': 543,
+    # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+    # 'apartment.crawler.middlewares.ApartmentSpiderMiddleware': 543,
     'frontera.contrib.scrapy.middlewares.schedulers.SchedulerSpiderMiddleware': 999,
 }
 
-# Enable or disable downloader middlewares
-# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOAD_MAXSIZE = 10 * 1024 * 1024  # 10MB Maximum document size
+DOWNLOAD_TIMEOUT = 60
 DOWNLOADER_MIDDLEWARES = {
-#    'apartment.crawler.middlewares.ApartmentDownloaderMiddleware': 543,
+    # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+    # 'apartment.crawler.middlewares.ApartmentDownloaderMiddleware': 543,
     'frontera.contrib.scrapy.middlewares.schedulers.SchedulerDownloaderMiddleware': 999,
 }
 
-# Enable or disable extensions
-# See https://doc.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+EXTENSIONS = {
+    # See https://doc.scrapy.org/en/latest/topics/extensions.html
+    # 'scrapy.extensions.telnet.TelnetConsole': None,
+}
 
-# Configure item pipelines
-# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'apartment.crawler.pipelines.ApartmentPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+    # 'apartment.crawler.pipelines.ApartmentPipeline': 300,
+}
 
-# Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 4.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
+ROBOTSTXT_OBEY = True
 
-# Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 HTTPCACHE_ENABLED = True
 HTTPCACHE_EXPIRATION_SECS = 0
@@ -100,9 +61,10 @@ HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
-# FRONTERA
+# Frontera
 SCHEDULER = 'frontera.contrib.scrapy.schedulers.frontier.FronteraScheduler'
 FRONTERA_SETTINGS = 'apartment.crawler.frontera.settings'
 
+# Scrapinghub
 SHUB_PROJECT_ID=os.environ.get('SHUB_PROJECT_ID', '')
 SHUB_API_KEY=os.environ.get('SHUB_API_KEY', '')
